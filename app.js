31d5a0f44a946869fa1a2fbfcb0505410cffaa78
Berto Melgar <berto@melgargarcia.net>
2015-07-13 22:28:39 +0100

    media queries

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var partials = require('express-partials');
 //var users = require('./routes/users');
 
 var app = express();
@@ -14,12 +15,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(partials());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
